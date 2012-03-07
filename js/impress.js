7a7d3e71199867009aed9e120e1e397ee35f6995
Bartek Szopka <bartek.szopka+github@gmail.com>
2012-03-07 23:37:04 +0100

    throttle on keydown (arrows) causes scrolling on Firefox

--- js/impress.js
+++ js/impress.js
@@ -376,7 +376,7 @@
     };
     
     // keyboard navigation handler
-    document.addEventListener("keydown", throttle(function ( event ) {
+    document.addEventListener("keydown", function ( event ) {
         if ( event.keyCode == 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
             switch( event.keyCode ) {
                 case 33: ; // pg up
@@ -395,7 +395,7 @@
             
             event.preventDefault();
         }
-    }, 50), false);
+    }, false);
     
     // delegated handler for clicking on the links to presentation steps
     document.addEventListener("click", function ( event ) {
