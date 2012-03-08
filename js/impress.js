807943dadc266f57caea0eba6ae0e680c787b99c
Bartek Szopka <bartek.szopka+github@gmail.com>
2012-03-08 21:02:05 +0100

    keyboard handler updated - now triggering actions on keyup

--- js/impress.js
+++ js/impress.js
@@ -378,8 +378,17 @@
         };
     };
     
-    // keyboard navigation handler
+    // keyboard navigation handlers
+    
+    // prevent default keydown action when one of supported key is pressed
     document.addEventListener("keydown", function ( event ) {
+        if ( event.keyCode == 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
+            event.preventDefault();
+        }
+    }, false);
+    
+    // trigger impress action on keyup
+    document.addEventListener("keyup", function ( event ) {
         if ( event.keyCode == 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
             switch( event.keyCode ) {
                 case 33: ; // pg up
