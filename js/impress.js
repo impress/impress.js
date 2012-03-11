b59a4bf4d7cca990f913a18d4aedaf8b3598577e
Bartek Szopka <bartek.szopka+github@gmail.com>
2012-03-11 09:15:35 +0000

    "bugfix release 0.4.1"

--- js/impress.js
+++ js/impress.js
@@ -11,7 +11,7 @@
  *
  * ------------------------------------------------
  *  author:  Bartek Szopka
- *  version: 0.4
+ *  version: 0.4.1
  *  url:     http://bartaz.github.com/impress.js/
  *  source:  http://github.com/bartaz/impress.js/
  */
