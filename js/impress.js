c7f5771211b4f400ef8ae92204246a2537f24139
Bartek Szopka <bartek.szopka+github@gmail.com>
2012-03-21 08:05:18 +0000

    fixing clicking links on active steps

--- js/impress.js
+++ js/impress.js
@@ -604,8 +604,8 @@
         // delegated handler for clicking on step elements
         document.addEventListener("click", function ( event ) {
             var target = event.target;
-            // find closest step element
-            while ( !target.classList.contains("step") &&
+            // find closest step element that is not active
+            while ( !(target.classList.contains("step") && !target.classList.contains("active")) &&
                     (target !== document.documentElement) ) {
                 target = target.parentNode;
             }
