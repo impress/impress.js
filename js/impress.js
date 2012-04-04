8949aae00ea126151332749d44836f543ed5ff66
Bartek Szopka <bartek.szopka+github@gmail.com>
2012-04-04 18:23:23 +0000

    next version

--- js/impress.js
+++ js/impress.js
@@ -11,7 +11,7 @@
  *
  * ------------------------------------------------
  *  author:  Bartek Szopka
- *  version: 0.5.2
+ *  version: 0.5.3 (in development)
  *  url:     http://bartaz.github.com/impress.js/
  *  source:  http://github.com/bartaz/impress.js/
  */
