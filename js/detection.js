750b7fad17a4968b6863047f434cddd7f0da635f
Robin Ricard <ricard.robin@gmail.com>
2012-07-05 19:35:38 +0200

    impress feature-detector can start just after Modernizr's start
    We can start adapting display as soon as possible !

--- js/detection.js
+++ js/detection.js
@@ -1,13 +1,13 @@
 Modernizr.addTest('impress', function(){
-    var body = document.body;
+    var head = document.head; // We can't use body here !
     var ua = navigator.userAgent.toLowerCase();
     var test =
         // browser should support CSS 3D transtorms 
         ( Modernizr.csstransforms ) &&
         
         // and `classList` and `dataset` APIs
-        ( body.classList ) &&
-        ( body.dataset ) &&
+        ( head.classList ) &&
+        ( head.dataset ) &&
         
         // but some mobile devices need to be blacklisted,
         // because their CSS 3D support or hardware is not
