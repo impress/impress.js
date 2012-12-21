13fd156125a10f9eebc0b7111f94326ec0794ac0
aliva <ali.vakilzade@gmail.com>
2012-12-21 23:45:27 +0330

    don't change slides on tab or directional keys

--- js/impress.js
+++ js/impress.js
@@ -705,16 +705,17 @@
             if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
                 switch( event.keyCode ) {
                     case 33: // pg up
-                    case 37: // left
-                    case 38: // up
                              api.prev();
                              break;
-                    case 9:  // tab
                     case 32: // space
                     case 34: // pg down
+                             api.next();
+                             break;
+                    case 9:  // tab
+                    case 37: // left
+                    case 38: // up
                     case 39: // right
                     case 40: // down
-                             api.next();
                              break;
                 }
                 
