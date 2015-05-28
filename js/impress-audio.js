4013e489500a8f910af4dec3645b646d77b41186
jyyan <jyyan.tw@gmail.com>
2015-05-28 09:46:05 +0800

    1. Add play with vedio media after play end audio. 2. Add autorun to next slide with the defined sec. time. using ** impress().play(TIME_SEC) **

--- js/impress-audio.js
+++ js/impress-audio.js
@@ -8,38 +8,69 @@ License: Free General Public License (GPL)
 (function(document, $, undefined) {
   'use strict';
 
-  var isPlaying,
-  audio,
-  impressObj = impress(),
+  var isPlaying, waitTimeout ,
+    audio,video,
+    impressObj = impress(),
     impressGoto = impressObj.goto;
+  var TimerID;
 
   $(document).on('impress:stepenter', function(event,f) {
     var $currSlide = $(event.target);
     if(audio) {
       audio.pause();
     }
+    if(video) {
+      video.pause();
+    }
+
     audio = $currSlide.find('audio')[0];
+    video = $currSlide.find('video')[0];
+
+    // make it play audio first then try to play the video if exist
     if(audio) {
       audio.play();
       if(isPlaying && $currSlide[0] != $currSlide.parent().children().last()[0]) {
         audio.addEventListener('ended',function() {
-          impressObj.goto($currSlide.next());
+          if(!video){
+            impressObj.goto($currSlide.next());
+          }else{
+            video.play();
+          }
           audio.removeEventListener('ended');
         });
       } else {
         isPlaying = false;
       }
+    }
 
+    if(video) {
+      if(!audio){
+        video.play();
+      }
+      if(isPlaying && $currSlide[0] != $currSlide.parent().children().last()[0]) {
+        video.addEventListener('ended',function() {
+          impressObj.goto($currSlide.next());
+          video.removeEventListener('ended');
+        });
+      } else {
+        isPlaying = false;
+      }
+    }
+
+    clearTimeout(TimerID);
+    if(!audio && !video && waitTimeout > 0 ){
+      TimerID = setTimeout(function(){impressObj.goto($currSlide.next())}, waitTimeout );
     }
   });
 
-  impressObj.play = function() {
+  impressObj.play = function(SecTimeout) {
     isPlaying = true;
+    waitTimeout = parseInt(SecTimeout)*1000 ;
     this.goto(0);
   };
 
   impressObj.goto = function($el) {
-    if(isNaN($el)) {
+    if($el instanceof jQuery) {
       impressGoto($el.parent().children().index($el), $el.data('transition-duration'));
     } else {
       impressGoto($el);
