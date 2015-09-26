184565cc7bc4e8118a0c8df3660f543665c25703
and3rson <anderson.dunai@gmail.com>
2015-09-26 21:36:54 +0300

    Removed disabling mobile support.

--- js/impress.js
+++ js/impress.js
@@ -188,12 +188,7 @@
                            
                           // and `classList` and `dataset` APIs
                            ( body.classList ) &&
-                           ( body.dataset ) &&
-                           
-                          // but some mobile devices need to be blacklisted,
-                          // because their CSS 3D support or hardware is not
-                          // good enough to run impress.js properly, sorry...
-                           ( ua.search(/(iphone)|(ipod)/) === -1 );
+                           ( body.dataset );
     
     if (!impressSupported) {
         // we can't be sure that `classList` is supported
