d7784ee878a28a71ec7b4db3384a189da9f1a56b
Fagner Brack <github@fagnermartins.com>
2016-04-16 17:12:20 +1000

    Use shift + Tab to go back to the previous step

--- js/impress.js
+++ js/impress.js
@@ -724,16 +724,24 @@
         //   Well, the [tab] key by default navigates around focusable elements, so clicking
         //   it very often caused scrolling to focused element and breaking impress.js
         //   positioning. I didn't want to just prevent this default action, so I used [tab]
-        //   as another way to moving to next step... And yes, I know that for the sake of
-        //   consistency I should add [shift+tab] as opposite action...
+        //   as another way to moving to next step...
         document.addEventListener( "keyup", function( event ) {
+
+            // Handles Tab and Shift + Tab
+            if ( event.keyCode === 9 ) {
+              if ( event.shiftKey ) {
+                api.prev();
+              } else {
+                api.next();
+              }
+              return;
+            }
 
             if ( event.shiftKey || event.altKey || event.ctrlKey || event.metaKey ) {
                 return;
             }
 
-            if ( event.keyCode === 9 ||
-               ( event.keyCode >= 32 && event.keyCode <= 34 ) ||
+            if ( ( event.keyCode >= 32 && event.keyCode <= 34 ) ||
                ( event.keyCode >= 37 && event.keyCode <= 40 ) ) {
                 switch ( event.keyCode ) {
                     case 33: // Page up
@@ -741,7 +749,6 @@
                     case 38: // Up
                              api.prev();
                              break;
-                    case 9:  // Tab
                     case 32: // Space
                     case 34: // Page down
                     case 39: // Right
