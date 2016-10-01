53214e584915e521f9c89f2a3e05d20f0b2d90fc
Fagner Brack <github@fagnermartins.com>
2016-10-02 04:34:40 +1100

    Add encrypted sauce credentials with new key (#584)
    * Add encrypted sauce credentials with new key
    
    * Remove binary workaround in ".gitattributes"
    
    * Revert a5fd940cd9a32cf0e3cf66880f2b7350d516c426
    
    * Use the -out syntax instead of >>
    
    * Revert "Use the -out syntax instead of >>"
    
    This reverts commit 7caf1de18c9103ea9c7397c1c8f0a45c31850ec6.
    
    Is this commit causing a "npm: command not found" error in CircleCI?
    
    * Update sauce-encrypted-env-vars to use `export`.
    
    * Normalize `transformOrigin` result for IE11 and Safari.
    
    * Add webkit prefixes for Safari 8 test pass.

--- test/core_tests.js
+++ test/core_tests.js
@@ -45,15 +45,20 @@
       var canvasIsNotAStep = !canvas.classList.contains("step") && canvas.id === "";
       assert.ok( canvasIsNotAStep, "Canvas do not have step element data" );
 
-      actual = canvas.style.transform;
+      actual = canvas.style.webkitTransform || canvas.style.transform;
       expected = "rotateZ(0deg) rotateY(0deg) rotateX(0deg) translate3d(1000px, 0px, 0px)";
       assert.strictEqual( actual, expected, "canvas.style.transform initialized correctly" );
 
-      actual = canvas.style.transformOrigin;
+      // Normalize result for IE 11 and Safari.
+      actual = canvas.style.webkitTransformOrigin || canvas.style.transformOrigin;
       expected = "left top 0px";
+
+      if ( actual === "left top" || actual === "0% 0%" ) {
+        actual = expected;
+      }
       assert.strictEqual( actual, expected, "canvas.style.transformOrigin initialized correctly" );
 
-      actual = canvas.style.transformStyle;
+      actual = canvas.style.webkitTransformStyle || canvas.style.transformStyle;
       expected = "preserve-3d";
       assert.strictEqual( actual, expected, "canvas.style.transformStyle initialized correctly" );
 
