ccdb0a4e7abf64ca696e0b8bc68ba735c359c9a8
Leo Colisson <leo.colisson@gmail.com>
2019-12-07 04:05:52 +0100

    Fix typo in doc: autostart -> autoplay

--- src/plugins/media/media.js
+++ src/plugins/media/media.js
@@ -26,22 +26,22 @@
  *    overwritten for individual steps and media.
  *
  *    Examples:
- *    - data-media-autostart="true" data-media-autostop="true": start media on enter, stop on
+ *    - data-media-autoplay="true" data-media-autostop="true": start media on enter, stop on
  *      leave, restart from beginning when re-entering the step.
  *
- *    - data-media-autostart="true" data-media-autopause="true": start media on enter, pause on
+ *    - data-media-autoplay="true" data-media-autopause="true": start media on enter, pause on
  *      leave, resume on re-enter
  *
- *    - data-media-autostart="true" data-media-autostop="true" data-media-autopause="true": start
+ *    - data-media-autoplay="true" data-media-autostop="true" data-media-autopause="true": start
  *      media on enter, stop on leave (stop overwrites pause).
  *
- *    - data-media-autostart="true" data-media-autopause="false": let media start automatically
+ *    - data-media-autoplay="true" data-media-autopause="false": let media start automatically
  *      when entering a step and let it play when leaving the step.
  *
- *    - <div id="impress" data-media-autostart="true"> ... <div class="step"
- *      data-media-autostart="false">
+ *    - <div id="impress" data-media-autoplay="true"> ... <div class="step"
+ *      data-media-autoplay="false">
  *      All media is startet automatically on all steps except the one that has the
- *      data-media-autostart="false" attribute.
+ *      data-media-autoplay="false" attribute.
  *
  *  - Pro tip: Use <audio onended="impress().next()"> or <video onended="impress().next()"> to
  *    proceed to the next step automatically, when the end of the media is reached.
