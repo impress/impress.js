0112f04e1a410d7312dd9480ba7b56653157643c
tonegas <tonegas@gmail.com>
2020-04-27 20:04:32 +0200

    fixed some lint problems

--- src/plugins/subeffects/substep-effects.js
+++ src/plugins/subeffects/substep-effects.js
@@ -1,37 +1,37 @@
 /**
  * Substep effects Plugin
- * 
+ *
  *
  * The plugin adds the posibility to add custom classes at each substep to objects:
  *      When an object with class substep uses one of the following attributes,
  *      the following classes are added to certain objects.
- * 
+ *
  * The attributes are:
  *      - data-addonly-TOCLASS = "CLASS" : This attribute is used to apply certain
- *          classes in particular CLASS, CLASS-base, CLASS-before, CLASS-after, 
+ *          classes in particular CLASS, CLASS-base, CLASS-before, CLASS-after,
  *          following the rules explained in section classes.
- *      
- *      - data-addfrom-TOCLASS = "CLASS" and data-addto-TOCLASS = "CLASS": 
- *          this two attributes are used to apply certain classes from the substep 
+ *
+ *      - data-addfrom-TOCLASS = "CLASS" and data-addto-TOCLASS = "CLASS":
+ *          this two attributes are used to apply certain classes from the substep
  *          with the attribute data-addfrom-TOCLASS = "CLASS" to the substep with the attribute
  *          data-addto-TOCLASS = "CLASS" with the rules explained in the following.
- * 
+ *
  * The classes are:
- *      
+ *
  *      - CLASS-base: This class is added at step enter to each object reffered by TOCLASS
- * 
+ *
  *      - CLASS-before: This class is added at step enter to each obbject reffered by TOCLASS
  *          ad removed when the substep with the attribute data-addonly-TOCLASS = "CLASS" or
  *          data-addfrom-TOCLASS = "CLASS" is reached
- * 
- *      - CLASS-after: This class is added after the substep with the attribute 
+ *
+ *      - CLASS-after: This class is added after the substep with the attribute
  *          data-addonly-TOCLASS = "CLASS" or data-addto-TOCLASS = "CLASS" is reached.
- * 
- *      - CLASS: This class is added when the substep with the attribute 
- *          data-addonly-TOCLASS = "CLASS" is reached and removed efterwards 
- *          or between the substep with the attribute data-addfrom-TOCLASS = "CLASS" and 
+ *
+ *      - CLASS: This class is added when the substep with the attribute
+ *          data-addonly-TOCLASS = "CLASS" is reached and removed efterwards
+ *          or between the substep with the attribute data-addfrom-TOCLASS = "CLASS" and
  *          data-addto-TOCLASS = "CLASS".
- *          
+ *
  *      Some examples of all these features are presented in the file example.html
  *
  *
@@ -39,8 +39,6 @@
  * http://tonegas.com
  * Released under the MIT license.
  */
-
-
 
 ( function( document ) {
     "use strict";
@@ -83,7 +81,7 @@
     }
 
     /* Function for appling the classes at certain substep */
-    function applyOnlyClass( subElem ){
+    function applyOnlyClass( subElem ) {
         for ( var i = 0, atts = subElem.attributes, n = atts.length; i < n; i++ )  {
             let lenStr = stringAddClassOnlyNow.length;
             if ( stringAddClassOnlyNow + "-" === atts[ i ].nodeName.substring( 0, lenStr + 1  ) ) {
@@ -100,7 +98,7 @@
     }
 
     /* Function for appling the classes at from a certain substep */
-    function applyFromClass( subElem ){
+    function applyFromClass( subElem ) {
         for ( var i = 0, atts = subElem.attributes, n = atts.length; i < n; i++ )  {
             let lenStr = stringAddClassFromNow.length;
             if ( stringAddClassFromNow + "-" === atts[ i ].nodeName.substring( 0, lenStr + 1  ) ) {
@@ -116,7 +114,7 @@
     }
 
     /* Function for appling the classes at until a certain substep */
-    function applyToClass( subElem ){
+    function applyToClass( subElem ) {
         for ( var i = 0, atts = subElem.attributes, n = atts.length; i < n; i++ )  {
             let lenStr = stringAddClassToNow.length;
             if ( stringAddClassToNow + "-" === atts[ i ].nodeName.substring( 0, lenStr + 1  ) ) {
@@ -163,7 +161,7 @@
         } );
         /* Active the condition of the active substep */
         event.target.querySelectorAll( ".substep.substep-active" ).forEach( subElem => {
-            applyOnlyClass ( subElem );
+            applyOnlyClass( subElem );
             applyFromClass( subElem );
             applyToClass( subElem );
         } );
