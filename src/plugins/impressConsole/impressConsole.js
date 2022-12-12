7f6b1ab8fd049e4a2de09e4171f62030e6375f60
janis <jahu04@outlook.com>
2022-12-12 18:04:52 +0100

    remove code from other pr

--- src/plugins/impressConsole/impressConsole.js
+++ src/plugins/impressConsole/impressConsole.js
@@ -16,12 +16,6 @@
 ( function( document, window ) {
     'use strict';
 
-    var notes = window.document.getElementsByClassName( 'notes' );
-
-    for ( let items in Object.entries( notes ).values ) {
-        items.style.display = 'none';
-    }
-
     // TODO: Move this to src/lib/util.js
     var triggerEvent = function( el, eventName, detail ) {
         var event = document.createEvent( 'CustomEvent' );
